33e11da770650d8b7fb2f6dcbe994cbc7df522cd
Mzzqq <rezarifa661@gmail.com>
2024-01-22 17:41:08 +0700

    add tribonacci-sequence solution

--- tribonacci-sequence.go
+++ tribonacci-sequence.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+func Tribonacci(signature [3]float64, n int) []float64 {
+	result := make([]float64, n)
+	if n == 0 {
+		return result
+	}
 
+	copy(result, signature[:])
+
+	for i := 3; i < n; i++ {
+		result[i] = result[i-1] + result[i-2] + result[i-3]
+	}
+
+	return result
 }
