3e5454e7e7005ede9e4e2c57abf25769fa30e272
Mzzqq <rezarifa661@gmail.com>
2024-01-22 17:08:54 +0700

    add multiplication-table-for-number solution

--- multiplication-table-for-number.go
+++ multiplication-table-for-number.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func MultiTable(number int) string {
+	result := ""
+	for i := 1; i <= 10; i++ {
+		result += fmt.Sprint(i, " * ", number, " = ", i*number)
+		if i < 10 {
+			result += "\n"
+		}
+	}
+	return result
 }
