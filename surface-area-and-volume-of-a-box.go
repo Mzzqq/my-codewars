5dfeba6834de6bd8c464181ed136fe5041f0c855
Mzzqq <rezarifa661@gmail.com>
2024-01-22 16:35:57 +0700

    add surface-area-and-volume-of-a-box solution

--- surface-area-and-volume-of-a-box.go
+++ surface-area-and-volume-of-a-box.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
-
+func GetSize(w, h, l int) [2]int {
+	result := [2]int{}
+	result[0] = ((2 * l * w) + (2 * l * h) + (2 * w * h))
+	result[1] = w * h * l
+	return result
 }
