39edf54869b9a7d01ecea40b39f6ec4088a3aae3
Mzzqq <rezarifa661@gmail.com>
2024-02-15 22:49:50 +0700

    add solution

--- I love you, a little , a lot, passionately ... not at all.go
+++ I love you, a little , a lot, passionately ... not at all.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
-
+func HowMuchILoveYou(i int) string {
+	if i == 6 || i%6 == 0 {
+		return "not at all"
+	} else {
+		switch res := i % 6; res {
+		case 1:
+			return "I love you"
+		case 2:
+			return "a little"
+		case 3:
+			return "a lot"
+		case 4:
+			return "passionately"
+		case 5:
+			return "madly"
+		}
+	}
+	return ""
 }
