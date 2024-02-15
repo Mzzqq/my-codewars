0750ec201ec8ae9d58b6da295b45678b77a89a57
Mzzqq <rezarifa661@gmail.com>
2024-02-15 22:38:24 +0700

    add solution

--- a-rule-of-divisibility-by-seven.go
+++ a-rule-of-divisibility-by-seven.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
-
+func Seven(n int64) []int {
+	step := 0
+	for n >= 100 {
+		step++
+		lastDigit := n % 10
+		n = (n - lastDigit) / 10
+		n -= 2 * lastDigit
+	}
+	return []int{int(n), step}
 }
