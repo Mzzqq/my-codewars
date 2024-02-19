455698bb4caa1ec8af3498c6edc459dfbf150349
Mzzqq <rezarifa661@gmail.com>
2024-02-20 00:13:07 +0700

    add solution

--- speed-control.go
+++ speed-control.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "math"
 
+func Gps(s int, x []float64) int {
+	if len(x) <= 1 {
+		return 0
+	}
+
+	maxSpeed := 0.0
+	for i := 1; i < len(x); i++ {
+		deltaDistance := x[i] - x[i-1]
+		speed := (3600 * deltaDistance) / float64(s)
+		maxSpeed = math.Max(maxSpeed, speed)
+	}
+
+	return int(math.Floor(maxSpeed))
 }
