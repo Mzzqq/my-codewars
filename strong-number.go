7e0798d4999f3c2cb99396bc6552458901fc3d2e
Mzzqq <rezarifa661@gmail.com>
2024-02-20 14:02:27 +0700

    add solution

--- strong-number.go
+++ strong-number.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
-
+func Strong(n int) string {
+	sum := 0
+	original := n
+	for n > 0 {
+		factorial := 1
+		for i := 1; i <= n%10; i++ {
+			factorial *= i
+		}
+		sum += factorial
+		n /= 10
+	}
+	if sum == original {
+		return "STRONG!!!!"
+	}
+	return "Not Strong !!"
 }
